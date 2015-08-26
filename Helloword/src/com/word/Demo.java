20696d0d7ea2ec4784cad8b1a9f49cac743c6b60
kmmuser <maheshkumar.palanisamy@imeddoc.ie>
2015-08-26 14:26:57 +0530

    Initial checkin

---
+++ Helloword/src/com/word/Demo.java
@@ -0,0 +1,9 @@
+package com.word;
+
+public class Demo {
+
+	public void numberDisplay(){
+		int b=20;
+		System.out.print("The Value B :"+b);
+	}
+}
---
+++ Helloword/src/com/word/Hello.java
@@ -0,0 +1,16 @@
+package com.word;
+
+public class Hello {
+
+	public static void main(String args[]){
+		
+		Demo obj = new Demo();
+		System.out.println("Hello");
+		number();
+		obj.numberDisplay();
+	}
+	public static void number(){
+		int a =10;
+		System.out.println("The Value A :"+a);
+	}
+}
